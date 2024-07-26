98c29e83097422443bc51df32066715e6b094ffa
rajendra0968jangid <rajendra0968jangid@gmail.com>
2024-07-26 09:37:31 +0530

    lat long

--- fronted/src/Components/Main.jsx
+++ fronted/src/Components/Main.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import About from "./Main/About";
 import Book from "./Main/Book";
 import Client from "./Main/Client";
@@ -6,10 +7,39 @@ import Footer from "./Main/Footer";
 import Header from "./Main/Header";
 import Offer from "./Main/Offer";
 function Main() {
+  const [location, setLocation] = useState({ latitude: null, longitude: null });
+
+  useEffect(() => {
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setLocation({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            });
+          },
+          (error) => {
+            console.error("Error getting location:", error);
+          }
+        );
+      } else {
+        console.error("Geolocation is not supported by this browser.");
+      }
+    };
+
+    getLocation();
+  }, []);
+
+  console.log(location);
   return (
     <>
       <main id="main">
         <Header />
+        <div>
+          <h1>latitude:{location.latitude || ""}</h1>
+          <h1>longitude:{location.longitude || ""}</h1>
+        </div>
         <Offer />
         <Food />
         <About />
